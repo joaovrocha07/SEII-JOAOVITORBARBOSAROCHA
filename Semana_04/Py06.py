224d37a2a0033125b52dccedfd477ebf6228852d
joaovrocha07 <105607480+joaovrocha07@users.noreply.github.com>
2022-06-05 14:54:22 -0300

    Update Py06.py

--- Semana_04/Py06.py
+++ Semana_04/Py06.py
@@ -1,42 +1,56 @@
-nums = [1, 2, 3, 4, 5]
-for num in nums:
-    if num == 3:
-        print('Found')
-        break
-    print(num)
+language = 'Python'
 
-nums = [1, 2, 3, 4, 5]
-for num in nums:
-    if num == 3:
-        print('Found')
-        continue
-    print(num)
+# if-else statement
+if language == 'Python':
+    print('Language is Python')
+else:
+    print('No match')
 
-nums = [1, 2, 3, 4, 5]
-for num in nums:
-    for letter in 'abc':
-        print(num, letter)
-for i in range(1, 11):
-    print(i)
+# elif statement
+language = 'Java'
+if language == 'Python':
+    print('Language is Python')
+elif language == 'Java':
+    print('Language is Java')
+elif language == 'JavaScript':
+    print('Language is JavaScript')
+else:
+    print('No match')
 
-for i in range(1, 11): #the last value is not inclusive
-    print(i)
+# Using the and operator
+user = 'Admin'
+logged_in = True
+if user == 'Admin' and logged_in:
+    print('Admin Page')
+else:
+    print('Bad Creds')
 
-x = 0
-while x < 10:
-    print(x)
-    x+=1
+# Using the not operator
+user = 'Admin'
+logged_in = False
+if not logged_in:
+    print('Please Log In')
+else:
+    print('Welcome')
 
-x = 0
-while x < 10:
-    if x == 5:
-        break
-    print(x)
-    x+=1
 
-x = 0
-while True:
-    if x == 5:
-        break
-    print(x)
-    x+=1 
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(id(a))
+print(id(b))
+print(a == b) #True
+print(a is b) #False -> 'is' looks for the same ID, same object in memory
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
+
+condition = False
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
+
