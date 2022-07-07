a058e8d1a138f7486ad44cbc669a21be63a376e2
joaovrocha07 <105607480+joaovrocha07@users.noreply.github.com>
2022-07-06 21:15:19 -0300

    Semana 09

---
+++ Semana_09/Social_Media/app.py
@@ -0,0 +1,29 @@
+#João Vitor Barbosa Rocha 11711EMT007
+
+#É necessário instalar os pacotes.
+
+from flask import Flask, render_template, request
+
+
+from flask_cors import CORS
+from models import create_post, get_posts
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+	if request.method == 'GET':
+		pass
+	if request.method == 'POST':
+		name = request.form.get('name')
+		post = request.form.get('post')
+		create_post(name,post)
+
+	posts = get_posts()
+
+	return render_template('index.html', posts=posts)
+
+if __name__ == '__main__':
+	app.run(debug = True)
